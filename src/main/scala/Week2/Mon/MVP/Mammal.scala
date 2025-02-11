deff3472662b942a8f27ff8a518fcb6807c46028
Nimo Mohamed <138133949+nimo-mohamed@users.noreply.github.com>
2025-02-11 10:41:09 +0000

    just to be sure

--- src/main/scala/Week2/Mon/MVP/Mammal.scala
+++ src/main/scala/Week2/Mon/MVP/Mammal.scala
@@ -4,3 +4,5 @@ trait Mammal {
 val furColour: String = "brown"
   def hasTail: Boolean = true
 }
+// val if its facts or discription
+// if you want it to do something because you get your answer its a method
